42fd8f6979f496c0999443e97a0f51396c066f85
administrator <administrator@ubuntu.(none)>
2013-07-13 16:03:34 -0400

    modified web.js

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });p.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
